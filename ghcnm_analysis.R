65d90147b9a2cf6a9a157cb69d4fe57cadde4bdb
vbengr <lego_fans@yahoo.com>
2014-05-21 19:07:59 -0400

    Moved plotting functions from ghcnm_data to ghcnm_analysis Created plots for selected stations

--- ghcnm_analysis.R
+++ ghcnm_analysis.R
@@ -1,25 +1,60 @@
 library(maps)
 rm(list=ls())
 
+# Select average temperature data
 ttype <- "tavg"
-
 # Load data
 if (!file.exists(paste0(ttype,"_out.RData"))) stop("File not found")
 load(paste0(ttype,"_out.RData"))
 
-out$LATRANGE <- cut(out$LATITUDE,seq(-90,90,30))
-out$ELEVRANGE <- cut(out$GRELEV,seq(0,3000,1000))
+# Append a trend attribute to the inventory. If p-value is > 0.05, slope is
+# insignificant -> No trend. Print output table
+inv.out$Trend50 <- factor(ifelse(inv.out$pval50>0.05,"No Trend",
+                                 ifelse(inv.out$dT50>0,"Increasing","Decreasing")))
+inv.out$Trend100 <- factor(ifelse(inv.out$pval100>0.05,"No Trend",
+                                  ifelse(inv.out$dT100>0,"Increasing","Decreasing")))
+print(table(inv.out$Trend50))
+print(table(inv.out$Trend100))
 
+# Plot function
+plot_func <- function(i){
+  dat1 <- dat.out[dat.out$ID==i,]
+  inv1 <- inv.out[inv.out$ID==i,]
+  main.title <- paste("Station ",inv1$ID," Lat = ",inv1$LATITUDE,
+                      " Long = ",inv1$LONGITUDE)
+  plot(dat1$YEAR,dat1$AVGTEMP,type="b",main=main.title,
+       ylab="Temperature Anomaly, K",xlab="Year",
+       xlim=c(1900,2014),ylim=c(-2,2))
+  # Best fit line for 50-year trend
+  a = inv.out$T50[inv.out$ID==i]
+  b = inv.out$dT50[inv.out$ID==i]
+  abline(a,b,col=2,lwd=2) 
+  # Best fit line for 100-year trend where computed
+  a = inv.out$T100[inv.out$ID==i]
+  b = inv.out$dT100[inv.out$ID==i]
+  if (!is.na(b)) abline(a, b,col=3,lwd=2)
+  legend("topleft",legend=c("50-year Trend","100-year Trend"),
+         lty=1,lwd=2,col=2:3,cex=0.8)
+}
+
+# Prepare plots for all stations
+# Open graphics output file
+pdf(file=paste0(ttype,"-plots.pdf"))
 par(las=1,cex.axis=1)
+for (i in levels(dat.out$ID)){
+  plot_func(as.character(i))
+}
+dev.off()
 
-#boxplot(dT~reorder(STVEG, dT, median), data=out,horizontal=TRUE,notch=TRUE)
-
-out$Trend50 <- factor(ifelse(out$pval50>0.05,"No Trend",
-                           ifelse(out$dT50>0,"Increasing","Decreasing")))
-out$Trend100 <- factor(ifelse(out$pval100>0.05,"No Trend",
-                           ifelse(out$dT100>0,"Increasing","Decreasing")))
-#out$dTRange <- cut(out$dT,-3:4)
+# Prepare metafile plots for selected stations as examples
+selID <- grep("BATES CREEK|PORT ELIZ|HARARE",inv.out$NAME)
+png(file="SelPlots.png",width=480,height=960)
+par(mfrow=c(3,1))
+for (i in selID){
+  plot_func(as.character(inv.out$ID[i]))
+}
+dev.off()
 
 map("world",col="gray")
 palette(c("blue","red","green"))
-points(out$LONGITUDE,out$LATITUDE,pch=19,col=out$Trend50,cex=0.5)
+points(inv.out$LONGITUDE,inv.out$LATITUDE,pch=19,col=inv.out$Trend50,cex=0.5)
--- ghcnm_data.R
+++ ghcnm_data.R
@@ -1,7 +1,7 @@
 rm(list=ls())
 # Calculate the annual average temperature change from monthly average, minimum or
 # maximum data over the past 50 (or 100) years at all stations with more than 
-# 50 (or 100) years of data after 1950 (or 1900) and prepares plots.
+# 50 (or 100) years of data after 1950 (or 1900) and write data to output files.
 
 for (ttype in c("tavg","tmin","tmax")){
   fn <- paste0(ttype,"-qca.RData")
@@ -14,10 +14,13 @@ for (ttype in c("tavg","tmin","tmax")){
   selSta <- which(table(dat$ID[dat$YEAR>1950])>=50)
   dat <- dat[dat$ID %in% levels(dat$ID)[selSta],]
   dat$ID <- factor(dat$ID)
-  # Add columns to inventory dataframe for temperature change
-  inv$dT50  <- inv$dT100 <- inv$pval50 <- inv$pval100 <- rep(NA,nrow(inv))
-  # Open graphics output file
-  pdf(file=paste0(ttype,"-plots.pdf"))
+  # Add columns to inventory dataframe for regression p-value, annual temperature 
+  # change (slope dT50/dT100) and intercept (T50/T100) of best fit line
+  inv$pval50 <- inv$pval100 <- rep(NA,nrow(inv))
+  inv$dT50  <- inv$dT100 <- rep(NA,nrow(inv))
+  inv$T50  <- inv$T100 <- rep(NA,nrow(inv))
+  # Create empty data frame
+  dat.out <- data.frame(ID=character(0),YEAR=integer(0),AVGTEMP=numeric(0))
   # Loop through all selected stations
   for (i in levels(dat$ID)){
     dat1 <- dat[dat$ID==i,]
@@ -27,34 +30,27 @@ for (ttype in c("tavg","tmin","tmax")){
     # Compute temperature anomaly by subtracting average from monthly value
     dat1[,4:15] <- t(apply(dat1[,4:15],1,function(x){x-avg}))/100
     # Compute annual average anomalies for the stattion
-    dat1$ann <- apply(dat1[,4:15],1,mean,na.rm=TRUE)
+    dat1$AVGTEMP <- apply(dat1[,4:15],1,mean,na.rm=TRUE)
+    # Write normalized data to output
+    dat.out <- rbind(dat.out,data.frame(ID=i,YEAR=dat1$YEAR,AVGTEMP=dat1$AVGTEMP))
     # If sufficient data, determine best fit line for the past period
     # At least 50 years of data since 1950
-    lm1 <- lm(ann~YEAR,data=dat1[dat1$YEAR>=1964&dat1$YEAR<=2013,])
-    # Multiply slope of best fit line  (deg per year) by 10 to obtain 
-    # the decadal temperature change and write to inventory dataframe
-    inv$dT50[inv$ID==i] <- coef(lm1)[2]*10
+    lm1 <- lm(AVGTEMP~YEAR,data=dat1[dat1$YEAR>=1964&dat1$YEAR<=2013,])
+    # Write slope = annual temperature change and intercept to inventory dataframe
+    inv$T50[inv$ID==i] <- coef(lm1)[1]
+    inv$dT50[inv$ID==i] <- coef(lm1)[2]
     # Extract regression p-value and write to inventory dataframe
     inv$pval50[inv$ID==i] <- summary(lm1)$coef[2,4]
     # At least 100 years of data
     if (nrow(dat1)>=100){
-      lm2 <- lm(ann~YEAR,data=dat1[dat1$YEAR>=1914&dat1$YEAR<=2013,])
-      # Multiply slope of best fit line  (deg per year) by 10 to obtain 
-      # the decadal temperature change and write to inventory dataframe
-      inv$dT100[inv$ID==i] <- coef(lm2)[2]*10
-      # Extract regression p-value and write to inventory dataframe
+      lm2 <- lm(AVGTEMP~YEAR,data=dat1[dat1$YEAR>=1914&dat1$YEAR<=2013,])
+      inv$T100[inv$ID==i] <- coef(lm2)[1]
+      inv$dT100[inv$ID==i] <- coef(lm2)[2]
       inv$pval100[inv$ID==i] <- summary(lm2)$coef[2,4]
     }
-    # Create a plot of the data with regression line
-    main.title <- paste(inv1$NAME,"Lat = ",inv1$LATITUDE,
-                        "Long = ",inv1$LONGITUDE)
-    plot(dat1$YEAR,dat1$ann,type="b",main=main.title,
-        ylab="Temperature Anomaly, K",xlab="Year",
-        xlim=c(1900,2014))
-    if (!is.na(inv$dT50[inv$ID==i])) abline(lm1,col=2,lwd=2)
-    if (!is.na(inv$dT100[inv$ID==i])) abline(lm2,col=3,lwd=2)
   }
-dev.off()
-out <- inv[!is.na(inv$dT50),]
-save(file=paste0(ttype,"_out.RData"),list=c("out"))
+# Discard stations with no computed 50-year trend
+inv.out <- inv[!is.na(inv$dT50),]
+# Write time series data and inventory to output
+save(file=paste0(ttype,"_out.RData"),list=c("inv.out","dat.out"))
 }
